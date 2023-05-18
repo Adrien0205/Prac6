68751cc9c1c1a949bf8f9200823bb20448fa269b
Adrien <adrien0205@hotmail.com>
2023-05-18 12:51:47 +0930

    lol

--- prac6/VMTranslator.py
+++ prac6/VMTranslator.py
@@ -1,49 +1,52 @@
 class VMTranslator:
 
-def vm_push(segment, offset):
-    '''Generate Hack Assembly code for a VM push operation'''
-    if segment == 'constant':
-        # Push constant value directly onto the stack
-        return f"@{offset}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
-    elif segment == 'local':
-        # Push value from the local segment onto the stack
-        return f"@LCL\nD=M\n@{offset}\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
-    elif segment == 'argument':
-        # Push value from the argument segment onto the stack
-        return f"@ARG\nD=M\n@{offset}\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
-    elif segment == 'this':
-        # Push value from the this segment onto the stack
-        return f"@THIS\nD=M\n@{offset}\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
-    elif segment == 'that':
-        # Push value from the that segment onto the stack
-        return f"@THAT\nD=M\n@{offset}\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
-    elif segment == 'pointer':
-        # Push value from the pointer segment onto the stack
-        if offset == '0':
-            return f"@THIS\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
-        elif offset == '1':
-            return f"@THAT\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
-    elif segment == 'temp':
-        # Push value from the temp segment onto the stack
-        return f"@R{5 + int(offset)}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
-    elif segment == 'static':
-        # Push value from the static segment onto the stack
-        return f"@{filename}.{offset}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+    @staticmethod
+    def vm_push(segment, offset):
+        '''Generate Hack Assembly code for a VM push operation'''
+        if segment == 'constant':
+            # Push constant value directly onto the stack
+            return f"@{offset}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+        elif segment == 'local':
+            # Push value from the local segment onto the stack
+            return f"@LCL\nD=M\n@{offset}\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+        elif segment == 'argument':
+            # Push value from the argument segment onto the stack
+            return f"@ARG\nD=M\n@{offset}\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+        elif segment == 'this':
+            # Push value from the this segment onto the stack
+            return f"@THIS\nD=M\n@{offset}\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+        elif segment == 'that':
+            # Push value from the that segment onto the stack
+            return f"@THAT\nD=M\n@{offset}\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+        elif segment == 'pointer':
+            # Push value from the pointer segment onto the stack
+            if offset == '0':
+                return f"@THIS\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+            elif offset == '1':
+                return f"@THAT\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+        elif segment == 'temp':
+            # Push value from the temp segment onto the stack
+            return f"@R{5 + int(offset)}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+        elif segment == 'static':
+            # Push value from the static segment onto the stack
+            return f"@{filename}.{offset}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
 
-def vm_pop(segment, offset):
-    '''Generate Hack Assembly code for a VM pop operation'''
-    if segment == 'local':
-        # Pop value from the stack and store it in the local segment
-        return f"@LCL\nD=M\n@{offset}\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n"
-    elif segment == 'argument':
-        # Pop value from the stack and store it in the argument segment
-        return f"@ARG\nD=M\n@{offset}\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n"
-    elif segment == 'this':
-        # Pop value from the stack and store it in the this segment
-        return f"@THIS\nD=M\n@{offset}\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n"
-    elif segment == 'that':
-        # Pop value from the stack and store it in the that segment
-        return f
+    @staticmethod
+    def vm_pop(segment, offset):
+        '''Generate Hack Assembly code for a VM pop operation'''
+        if segment == 'local':
+            # Pop value from the stack and store it in the local segment
+            return f"@LCL\nD=M\n@{offset}\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n"
+        elif segment == 'argument':
+            # Pop value from the stack and store it in the argument segment
+            return f"@ARG\nD=M\n@{offset}\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n"
+        elif segment == 'this':
+            # Pop value from the stack and store it in the this segment
+            return f"@THIS\nD=M\n@{offset}\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n"
+        elif segment == 'that':
+            # Pop value from the stack and store it in the that segment
+            return f"@THAT\nD=M\n@{offset}\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n"
+
 
 
     def vm_add():
